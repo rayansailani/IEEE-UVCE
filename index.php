0aa3817490a2c73c8716f88a726d7e4c1237a8fe
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-01 16:29:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -78,13 +78,13 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="mainieeeuvce.jpg" alt="First slide">
+      <img class="d-block w-100" src="mainieeeuvce.jpg" alt="First slide" style="object-fit: cover;width: 100%;height: 600px;">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="mainieeeuvce.jpg" alt="Second slide">
+      <img class="d-block w-100" src="mainieeeuvce.jpg" alt="Second slide" style="object-fit: cover;width: 100%;height: 600px;">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="mainieeeuvce.jpg" alt="Third slide">
+      <img class="d-block w-100" src="mainieeeuvce.jpg" alt="Third slide" style="object-fit: cover;width: 100%;height: 600px;">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
