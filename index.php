c4ace860e8a95ae23e23c7abaab6ba8671e2b11b
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-01 20:52:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -148,6 +148,14 @@
 </div>    
 <br>
    
+
+<div class="container">
+    <h1 class="mt-4 mb-4">SIG's</h1>
+  <img src="ieeeuvce.png" alt="Avatar" class="image">
+  <div class="overlay">
+    <div class="text">Hello World</div>
+  </div>
+</div>    
     
     
   
@@ -174,6 +182,50 @@
   }).scroll(); //invoke scroll-handler on page-load
 });
   </script>
+    
+ 
+    
+ <style>
+.container {
+  position: relative;
+  width: 50%;
+}
+
+.image {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: #008CBA;
+}
+
+.container:hover .overlay {
+  opacity: 1;
+}
+
+.text {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+</style> 
     
     
     
