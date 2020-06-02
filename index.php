253f5f443a27a68f0d68e33575ed40c361b3b755
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-02 12:32:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -95,15 +95,15 @@
     <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="fest2.jpg"
+      <img class="d-block w-100 img-fluid" src="mainieeeuvce.jpg"
         alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="mainieeeuvce.jpg"
+      <img class="d-block w-100 img-fluid" src="mainieeeuvce.jpg"
         alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="fest1.jpg"
+      <img class="d-block w-100 img-fluid" src="mainieeeuvce.jpg"
         alt="Third slide">
     </div>
   </div>
