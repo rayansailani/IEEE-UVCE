ccbbb84dd9b65fc7a0a4e365b3ef338c17aa1f8e
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-02 11:46:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -92,41 +92,54 @@
 
     
 <!--carousel-->  
-  <!-- Slideshow container -->
-<div class="slideshow-container">
-
-  <!-- Full-width images with number and caption text -->
-  <div class="mySlides fade">
-    <div class="numbertext">1 / 3</div>
-    <img src="img1.jpg" style="width:100%">
-    <div class="text">Caption Text</div>
-  </div>
-
-  <div class="mySlides fade">
-    <div class="numbertext">2 / 3</div>
-    <img src="img2.jpg" style="width:100%">
-    <div class="text">Caption Two</div>
-  </div>
-
-  <div class="mySlides fade">
-    <div class="numbertext">3 / 3</div>
-    <img src="img3.jpg" style="width:100%">
-    <div class="text">Caption Three</div>
-  </div>
-
-  <!-- Next and previous buttons -->
-  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-  <a class="next" onclick="plusSlides(1)">&#10095;</a>
-</div>
-<br>
-
-<!-- The dots/circles -->
-<div style="text-align:center">
-  <span class="dot" onclick="currentSlide(1)"></span>
-  <span class="dot" onclick="currentSlide(2)"></span>
-  <span class="dot" onclick="currentSlide(3)"></span>
-</div>
-    
+ <section class="carousel slide cid-qBIAxuQ1lW" data-interval="false" id="slider1-1s" data-rv-view="6183">
+	<div class="full-screen">
+		<div class="mbr-slider slide carousel" data-pause="true" data-keyboard="false" data-ride="false" data-interval="false">
+			<ol class="carousel-indicators">
+				<li data-app-prevent-settings="" data-target="#slider1-1s" data-slide-to="0"></li>
+				<li data-app-prevent-settings="" data-target="#slider1-1s" data-slide-to="1"></li>
+				<li data-app-prevent-settings="" data-target="#slider1-1s" class=" active" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner" role="listbox">
+				<div class="carousel-item slider-fullscreen-image" data-bg-video-slide="false" style="background-image: url(assets2/images/mitchel-lensink-224719-2000x1333.jpg);">
+					<div class="container container-slide">
+						<div class="image_wrapper">
+							<div class="mbr-overlay" style="opacity: 0.3; background-color: rgb(0, 0, 0);"></div>
+							<img src="mainieeeuvce.jpg">
+							<div class="carousel-caption justify-content-center">
+								<div class="col-10 align-center">
+									<h2 class="mbr-fonts-style display-5">Sed felis magna, semper sed ligula eu, tristique pretium quam.</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-item slider-fullscreen-image" data-bg-video-slide="false" style="background-image: url(assets2/images/jeremy-perkins-307186-2000x1333.jpg);">
+					<div class="container container-slide">
+						<div class="image_wrapper">
+							<div class="mbr-overlay" style="background-color: rgb(35, 35, 35);"></div>
+							<img src="mainieeeuvce.jpg">
+							<div class="carousel-caption justify-content-center">
+								<div class="col-10 align-left"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-item slider-fullscreen-image active" data-bg-video-slide="false" style="background-image: url(assets2/images/mitchel-lensink-205264-2000x1333.jpg);">
+					<div class="container container-slide">
+						<div class="image_wrapper">
+							<div class="mbr-overlay"></div>
+							<img src="mainieeeuvce.jpg">
+							<div class="carousel-caption justify-content-center">
+								<div class="col-10 align-right"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div><a data-app-prevent-settings="" class="carousel-control carousel-control-prev" role="button" data-slide="prev" href="#slider1-1s"><span aria-hidden="true" class="mbri-left mbr-iconfont"></span><span class="sr-only">Previous</span></a><a data-app-prevent-settings="" class="carousel-control carousel-control-next" role="button" data-slide="next" href="#slider1-1s"><span aria-hidden="true" class="mbri-right mbr-iconfont"></span><span class="sr-only">Next</span></a>
+		</div>
+	</div>
+</section> 
     
 <!-- <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -309,102 +322,7 @@
 }
 </style> 
     
- <style>
-     * {box-sizing:border-box}
-
-/* Slideshow container */
-.slideshow-container {
-  max-width: 1000px;
-  position: relative;
-  margin: auto;
-}
-
-/* Hide the images by default */
-.mySlides {
-  display: none;
-}
-
-/* Next & previous buttons */
-.prev, .next {
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  width: auto;
-  margin-top: -22px;
-  padding: 16px;
-  color: white;
-  font-weight: bold;
-  font-size: 18px;
-  transition: 0.6s ease;
-  border-radius: 0 3px 3px 0;
-  user-select: none;
-}
-
-/* Position the "next button" to the right */
-.next {
-  right: 0;
-  border-radius: 3px 0 0 3px;
-}
-
-/* On hover, add a black background color with a little bit see-through */
-.prev:hover, .next:hover {
-  background-color: rgba(0,0,0,0.8);
-}
-
-/* Caption text */
-.text {
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  position: absolute;
-  bottom: 8px;
-  width: 100%;
-  text-align: center;
-}
-
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
-
-/* The dots/bullets/indicators */
-.dot {
-  cursor: pointer;
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-
-.active, .dot:hover {
-  background-color: #717171;
-}
-
-/* Fading animation */
-.fade {
-  -webkit-animation-name: fade;
-  -webkit-animation-duration: 1.5s;
-  animation-name: fade;
-  animation-duration: 1.5s;
-}
-
-@-webkit-keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
-}
-
-@keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
-}
-    </style>
+ 
     
     
     <!-- Footer -->
