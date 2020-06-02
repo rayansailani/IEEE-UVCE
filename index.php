a235a831fcf88ac7d4bf50a8b83bf47cbf4356ae
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-02 12:13:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -91,52 +91,9 @@
     
 
     
-<!--carousel-->  
-<!--Carousel Wrapper-->
-    <div id="carousel-example-1z" class="carousel slide carousel-fade z-depth-1-half" data-ride="carousel">
-      <!--Indicators-->
-      <ol class="carousel-indicators">
-        <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-1z" data-slide-to="1"></li>
-        <li data-target="#carousel-example-1z" data-slide-to="2"></li>
-      </ol>
-      <!--/.Indicators-->
-      <!--Slides-->
-      <div class="carousel-inner" role="listbox">
-        <!--First slide-->
-        <div class="carousel-item active">
-          <img class="d-block w-100" src="mainieeeuvce.jpg" alt="First slide">
-        </div>
-        <!--/First slide-->
-        <!--Second slide-->
-        <div class="carousel-item">
-          <img class="d-block w-100" src="mainieeeuvce.jpg" alt="Second slide">
-        </div>
-        <!--/Second slide-->
-        <!--Third slide-->
-        <div class="carousel-item">
-          <img class="d-block w-100" src="mainieeeuvce.jpg" alt="Third slide">
-        </div>
-        <!--/Third slide-->
-      </div>
-      <!--/.Slides-->
-      <!--Controls-->
-      <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-      <!--/.Controls-->
-    </div>
-    <!--/.Carousel Wrapper-->
- 
+<!--carousel-->
     
-    
-    
-<!--<ol class="carousel-indicators">
+<ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
@@ -161,7 +118,7 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
-<br>-->
+<br>
     
    <!--About-->
 <div id="about" class="m-5">
