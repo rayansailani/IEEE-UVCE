d085657b6a138f8a1d17269bbc0332a9d4effad4
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-02 12:20:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -92,33 +92,31 @@
 
     
 <!--carousel-->
-    
-<ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="mainieeeuvce.jpg" alt="First slide" style="width:100%">
+      <img class="d-block w-100" src="mainieeeuvce.jpg"
+        alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="mainieeeuvce.jpg" alt="Second slide" style="width:100%">
+      <img class="d-block w-100" src="mainieeeuvce.jpg"
+        alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="mainieeeuvce.jpg" alt="Third slide" style="width:100%">
+      <img class="d-block w-100" src="mainieeeuvce.jpg"
+        alt="Third slide">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
 </div>
-<br>
+
     
    <!--About-->
 <div id="about" class="m-5">
