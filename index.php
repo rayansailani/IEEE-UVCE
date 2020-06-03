8feb9952388298b8042dfb2a569ef2aab12c3a21
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-03 21:44:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 <body style="background-color:#fff">
 	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
 		<a class="navbar-brand" href="#">
-			<img src="logo.png" width="40" height="40" alt="ieeeuvce">
+			<img src="assets/logo.png" width="40" height="40" alt="ieeeuvce">
 		</a>
 		<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse"> <span class="navbar-toggler-icon"></span>
 		</button>
