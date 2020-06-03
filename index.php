60849c5d7da8fc15643848f0ca6fbfd6e88550f8
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-03 15:23:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -163,6 +163,31 @@
 		</div>
 	</div>
 	<br>
+	
+	<!--Gallery-->
+	<div class="container">
+
+  <h1 class="font-weight-light text-center text-lg-left mt-4 mb-0">allery</h1>
+
+  <hr class="mt-2 mb-5">
+
+  <div class="row text-center text-lg-left">
+	  
+	<? for($i=0;$i<8;$i++) { ?>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="ieeeuvce.png" alt="">
+          </a>
+    </div>
+ <? } ?>
+	  
+  </div>
+
+</div>
+	
+	
+	
+	
 	<!--faculty-->
 	
 	
