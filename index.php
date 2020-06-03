801b4f4e3e2bfdb4b1e9d376d228639f2df5d33e
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-03 12:43:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -79,16 +79,16 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
   <div class="carousel-inner">
-			<div class="carousel-item active">
-					<img src="mainieeeuvce.jpg" alt="First slide" class="d-block img-fluid caro">
-			</div>
-			<div class="carousel-item active">
-					<img src="caro2.jpg" alt="First slide" class="d-block img-fluid caro">
-			</div>
-			<div class="carousel-item active">
-					<img src="mainieeeuvce.jpg" alt="First slide" class="d-block img-fluid caro">
-	  </div>
-</div>
+    <div class="carousel-item active">
+      <img src="mainieeeuvce.jpg" alt="First slide" class="d-block img-fluid caro">
+    </div>
+    <div class="carousel-item">
+      <img src="caro2.jpg" alt="First slide" class="d-block img-fluid caro">
+    </div>
+    <div class="carousel-item">
+      <img src="mainieeeuvce.jpg" alt="First slide" class="d-block img-fluid caro">
+    </div>
+  </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
@@ -98,6 +98,9 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
+	
+	
+	
 	
 	
 	
