7723520afbe9b93da8580d3b8fb0f25d1ac82aa3
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-03 16:38:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -191,7 +191,26 @@
   </div>
 
 </div>
-	
+<br>
+	<div style="cursor: pointer;">
+	<div class="accordion" id="accordionExample">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h2 class="mb-0">
+        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Gallery
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+        Skateboard mask
+      </div>
+    </div>
+  </div>
+	</div>
+</div>
 	
 	
 	
