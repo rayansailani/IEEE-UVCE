6dd2446ccd7b8255be5c997b74eabe4ada460875
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-03 22:54:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -131,8 +131,81 @@
 			<? } ?>
 	</div>
 	
+	<!--Team-->
+	<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="container carousel-inner no-padding">
+    <div class="carousel-item active">
+      <div class="col-xs-3 col-sm-3 col-md-3">
+        <img src="assets/carousel2.jpg">
+      </div>    
+      <div class="col-xs-3 col-sm-3 col-md-3">
+        <img src="assets/carousel2.jpg">
+      </div>   
+      <div class="col-xs-3 col-sm-3 col-md-3">
+        <img src="assets/carousel2.jpg">
+      </div>   
+      <div class="col-xs-3 col-sm-3 col-md-3">
+        <img src="assets/carousel2.jpg">
+      </div> 
+      <div class="col-xs-3 col-sm-3 col-md-3">
+        <img src="assets/carousel2.jpg">
+      </div>  
+    </div>
+    <div class="carousel-item">
+      <div class="col-xs-3 col-sm-3 col-md-3">
+        <img src="assets/carousel1.jpg">
+      </div>    
+      <div class="col-xs-3 col-sm-3 col-md-3">
+        <img src="assets/carousel1.jpg">
+      </div>   
+      <div class="col-xs-3 col-sm-3 col-md-3">
+        <img src="assets/carousel1.jpg">
+      </div>   
+      <div class="col-xs-3 col-sm-3 col-md-3">
+        <img src="assets/carousel1.jpg">
+      </div>
+      <div class="col-xs-3 col-sm-3 col-md-3">
+        <img src="assets/carousel1.jpg">
+      </div> 
+    </div>
+    <div class="carousel-item">
+      <div class="col-xs-3 col-sm-3 col-md-3">
+        <img src="assets/logo.png">
+      </div>    
+      <div class="col-xs-3 col-sm-3 col-md-3">
+        <img src="assets/logo.png">
+      </div>   
+      <div class="col-xs-3 col-sm-3 col-md-3">
+        <img src="assets/logo.png">
+      </div>   
+      <div class="col-xs-3 col-sm-3 col-md-3">
+        <img src="assets/logo.png">
+      </div>
+      <div class="col-xs-3 col-sm-3 col-md-3">
+        <img src="assets/logo.png">
+      </div> 
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
 	
-	<!--Fests-->
+	
 	
 	
 	<div class="fade" id="awards"></div>
