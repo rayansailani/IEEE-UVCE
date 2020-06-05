657896c1f4d342f55be5d8b1f752d4e8d051e875
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-05 17:31:05 +0530

    Create login.php

---
+++ login/login.php
@@ -0,0 +1,108 @@
+<?php 
+	include 'libraries/cakes.php'; 
+?>
+<!--HTML boiler plate-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/x-icon" sizes="192x192" href="../assets/logo.png">
+	<title>IEEE-UVCE</title>
+	<link rel="stylesheet" href="style.css">
+	<!--bootstrap link-->
+	<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<!--font awesome-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!--bootstrap js-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	<!--animate.css-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
+	<!-- Compiled and minified CSS -->
+	<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">-->
+	<!-- Compiled and minified JavaScript -->
+	<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>-->
+</head>
+
+<body style="background-color:#fff">
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+		<a class="navbar-brand" href="#">
+			<img src="../assets/logo.png" width="40" height="40" alt="ieeeuvce">
+		</a>
+		<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse"> <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarCollapse">
+			<div class="navbar-nav">
+					<a class="nav-link" href="../index.php">Home<span class="sr-only">(current)</span></a>
+					<a class="nav-link" href="register/register.php">Register</a>
+					<a class="nav-link" href="#">Login</a>
+			</div>
+		</div>
+	</nav>
+	
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  </body>
+  
+ <!-- Footer -->
+<footer class="page-footer font-small" style="background-color:#E3E2DF;">
+
+  <!-- Footer Links -->
+  <div class="container">
+
+    
+  
+
+    <!-- Grid row-->
+    <div class="row d-flex text-center justify-content-center mb-md-0 mb-4">
+
+      <!-- Grid column -->
+      <div class="col-md-8 col-12 mt-5">
+        <p style="line-height: 0.8rem"><a class="m-2" href="mailto:mahith.98@gmail.com " target="_blank"><i class="fa fa-envelope"> </i> Mahith Shetty</a><br><br><!--<a>Phone : +91 9845207684</a></p>-->
+	      <a class="p-2" href="https://goo.gl/maps/P43RC7Qphrq" target="_blank">				
+                        <i class="fa fa-map-marker"></i> UVCE, K.R Circle, B.R Ambedkar Veedhi, Bengaluru	                        
+                    </a>
+      </div>
+				                   
+      <!-- Grid column -->
+
+    </div>
+    
+      <!-- Grid column -->
+
+
+  </div>
+	<div class="container">
+		<div class="text-center">
+			<i class="fa fa-facebook m-2"></i>		
+			<i class="fa fa-youtube m-2"></i>		
+			<i class="fa fa-twitter m-2"></i>		
+			<i class="fa fa-linkedin m-2"></i>		
+			<i class="fa fa-instagram m-2"></i>
+		</div>
+	</div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© Copyright:
+    <a href="../index.php">ieeeuvce</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+</html>
