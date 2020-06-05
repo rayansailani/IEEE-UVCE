3e23accc21b7a441732a325222abbc9ae9345ed3
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-05 20:23:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" type="image/x-icon" sizes="192x192" href="assets/logo.png">
 	<title>IEEE-UVCE</title>
-	<link rel="stylesheet" href="refine/style.css">
-	<script src="refine/features.js"></script>
+	<link rel="stylesheet" href="style.css">
+	<script src="features.js"></script>
 	<!--bootstrap link-->
 	<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
       integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">-->
