df493a2fd5c1b648b072bf17fc227417e94bf271
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-06 21:35:53 +0530

    Update admin_enter.php

--- pages/admin_enter.php
+++ pages/admin_enter.php
@@ -65,6 +65,7 @@
 	  <div class="text-center">
 		  <?php if(isset($_SESSION['user_name'])&&$_SESSION['user_name']=="root"){
                         echo '<a class="btn btn-dark btn-sm m-4" href="create_event.php" role="button">Create a new event</a>';
+			echo '<a class="btn btn-dark btn-sm m-4" href="modify_event.php" role="button">Modify event</a>';
                     }
                     else
                     {
