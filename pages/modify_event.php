318bd4517ffb6f93160c16a051b6c9e5748d1df0
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-07 16:00:30 +0530

    Update modify_event.php

--- pages/modify_event.php
+++ pages/modify_event.php
@@ -78,6 +78,25 @@
 	
 	$c=count($title);
 	
+	
+	
+	
+	$events2=Array();
+	$res2 = $con->query("select * from old_event");
+	while($ele2 = $res2->fetch_assoc())
+		$events2[]=$ele2;
+	
+	
+	$title2 = array();
+	foreach($events2 as $eve2)
+		$title2[]=$eve2['title'];
+	
+	$dateandtime2 = array();
+	foreach($events2 as $eve2)
+		$dateandtime2[]=$eve2['dateandtime'];
+	
+	$c2=count($title);
+	
 ?>	
 	
 	
@@ -89,15 +108,18 @@
 		  	<? for($i=0;$i<$c;$i++) { ?>
 			    <div class="row">
 				    <div class="col m-2"><?=$title[$i]?></div>
-				    <div class="col m-2"><a class="btn btn-dark btn-sm" href="validate_event.php?name=delete&&title=<?=$title[$i]?>" role="button">delete</a></div>
-				    <div class="col m-2"><a class="btn btn-dark btn-sm" href="validate_event.php?name=move&&title=<?=$title[$i]?>" role="button">move to history</a></div>
+				    <div class="col m-2"><?=$dateandtime[$i]?></div>
+				    <div class="col m-2"><a class="btn btn-dark btn-sm" href="validate_event.php?name=delete&&title=<?=$title[$i]?>&&dateandtime=<?=$dateandtime[$i]?>" role="button">delete</a></div>
+				    <div class="col m-2"><a class="btn btn-dark btn-sm" href="validate_event.php?name=move&&title=<?=$title[$i]?>&&dateandtime=<?=$dateandtime[$i]?>" role="button">move to history</a></div>
 		  		</div>
 		  	<? } ?>
 		  <? } else if(isset($_SESSION['user_name'])&&$_SESSION['user_name']=="amigroot") { ?>
-		  	<? for($i=0;$i<$c;$i++) { ?>
+		  	<? for($i=0;$i<$c2;$i++) { ?>
 			    <div class="row">
 				    <div class="col m-2"><?=$title[$i]?></div>
 				    <div class="col m-2"><?=$dateandtime[$i]?></div>
+				     <div class="col m-2"><a class="btn btn-dark btn-sm" href="validate_event.php?name=delete&&title=<?=$title[$i]?>&&dateandtime=<?=$dateandtime[$i]?>" role="button">delete</a></div>
+				    <div class="col m-2"><a class="btn btn-dark btn-sm" href="validate_event.php?name=move&&title=<?=$title[$i]?>&&dateandtime=<?=$dateandtime[$i]?>" role="button">move to history</a></div>
 				    <div class="col m-2"><a class="btn btn-dark btn-sm" href="validate_event.php?name=deletehistory&&title=<?=$title[$i]?>&&dateandtime=<?=$dateandtime[$i]?>" role="button">delete</a></div>
 		  		</div>
 		  	<? } ?>
