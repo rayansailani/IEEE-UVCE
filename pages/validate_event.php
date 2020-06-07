b2c78260b878d7bce4d3b586eb3bfe2f2c5498c6
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-07 12:53:19 +0530

    Update validate_event.php

--- pages/validate_event.php
+++ pages/validate_event.php
@@ -128,7 +128,7 @@ if(isset($_POST['create_event'])){
 			if($fileSize<30000)
 			{
 				$fileNewName = $title.".".$fileActualExt;
-				$fileDestination='../assets/event_files/'.$fileNewName;
+				$fileDestination='uploads/'.$fileNewName;
 				move_uploaded_file($fileTmpName,$fileDestination);
 				echo "file uploaded";
 			}
