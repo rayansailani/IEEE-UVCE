db94dea96f2eeafa2ae78805bfd543117396b5e6
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-08 12:40:14 +0530

    Update advanced_editing.php

--- pages/advanced_editing.php
+++ pages/advanced_editing.php
@@ -108,7 +108,7 @@
                echo'<div class="container"><form method="POST" action="validate_event.php" enctype="multipart/form-data">
   <div class="form-row">
       <label for="validationServer01">Event title</label>
-      <input type="text" class="form-control" id="title" placeholder="Title" name="title" value='.$t.' required>
+      <input type="text" class="form-control" id="title" placeholder="Title" name="title" value='.$title.' required>
    </div>
     <div class="form-row">
       <label for="validationServer02">Presenters</label>
