a97e8997e039cc284d84f80078e71a27dd36e537
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-09 13:23:51 +0530

    Update validate_event.php

--- pages/validate_event.php
+++ pages/validate_event.php
@@ -241,15 +241,12 @@ else if($name=="update_arrange_id_history")
 	
 	
 }
-else if($name=="getids")
+
+
+
+if(isset($_POST['change_order']))
 {
-	/*print_r($arrange_id);echo "<br>";
-	print_r($title);echo "<br>";
-	print_r($dateandtime);echo "<br>";*/
-	isset($_POST['getids'])
-	{echo $name."<br>";
-	
-	print_r($_POST['arrange_id']);}
+	print_r($_POST['arrange_id']); 	
 }
 /*else if($name=="advanced_editing_current")
 {
