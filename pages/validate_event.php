6b15bbec68e7bcb2f6522472a73ac8f97672f9fe
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-11 00:53:21 +0530

    Update validate_event.php

--- pages/validate_event.php
+++ pages/validate_event.php
@@ -436,18 +436,15 @@ $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!'
 	
 	
 	
-	<?php
+	
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['upload_file']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['upload_file']['tmp_name'])) {
     // FIXME: you should add more of your own validation here, e.g. using ext/fileinfo
     try {
         // FIXME: you should not use 'name' for the upload, since that's the original filename from the user's computer - generate a random filename that you then store in your database, or similar
         $upload = $s3->upload($bucket, $_FILES['upload_file']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
-?>
-        <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
-<?php } catch(Exception $e) { ?>
-        <p>Upload error :(</p>
-<?php } } ?>
-	
+
+    }
+}
   
   	/*$file=$_FILES['upload_file'];
 	print_r($file);
