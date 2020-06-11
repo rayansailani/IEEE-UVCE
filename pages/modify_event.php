0ac70eb674d5a6d8ed91e9fcac17c8281d70e4b5
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-12 01:12:16 +0530

    Update modify_event.php

--- pages/modify_event.php
+++ pages/modify_event.php
@@ -99,7 +99,7 @@
 	////4////
 	
 	$events4=Array();
-	$res4 = $con->query("select * from new_event order by arrange_id asc");
+	$res4 = $con->query("select * from new_event order by arrange_id dsc");
 	while($ele4 = $res4->fetch_assoc())
 		$events4[]=$ele4;
 	
