e54a7047318fbfadf4273901f71145e3df5c8507
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-12 11:19:18 +0530

    Update modify_event.php

--- pages/modify_event.php
+++ pages/modify_event.php
@@ -194,7 +194,7 @@
 				    <div class="col m-2"><?=$title4[$i]?></div>
 				    <div class="col m-2"><?=$dateandtime4[$i]?></div>
 				    <div class="col m-2"><?=$arrange_id4[$i]?></div>
-				    <div class="col m-2"><input type="number" class="form-control" id="arrange_id" placeholder="arrange id" name="arrange_id[]" value="<?=$arrange_id4[$i]?>" ></div>
+				    <div class="col m-2"><input type="number" class="form-control" id="arrange_id" placeholder="arrange id" name="arrange_id[]" <!--value="<?=$arrange_id4[$i]?>"--> ></div>
 				    <div class="col m-2"><a class="btn btn-dark btn-sm" href="validate_event.php?name=delete_event&&title=<?=$title4[$i]?>&&dateandtime=<?=$dateandtime4[$i]?>&&event_id=<?=$event_id4[$i]?>" role="button">delete</a></div>
 				    <div class="col m-2"><a class="btn btn-dark btn-sm" href="validate_event.php?name=move_to_history&&title=<?=$title4[$i]?>&&dateandtime=<?=$dateandtime4[$i]?>&&event_id=<?=$event_id4[$i]?>&&filename=<?=$filename4[$i]?>" role="button">move to history</a></div>
 				     <div class="col m-2"><a class="btn btn-dark btn-sm" href="advanced_editing.php?name=advanced_editing_current&&title=<?=$title4[$i]?>&&dateandtime=<?=$dateandtime4[$i]?>&&$event_id=<?=$event_id4[$i]?>" role="button">Advanced Editing</a></div>
