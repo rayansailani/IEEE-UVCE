f76c019ef0e755e2539bde16db030ba26523c758
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-14 17:36:06 +0530

    Update team.php

--- pages/team.php
+++ pages/team.php
@@ -74,8 +74,8 @@
 	
 	<h1 class="mt-4 mb-4 text-center fade">Team IEEE UVCE</h1>
 	<? for($i=0;$i<11;$i++) { ?>
-		<div class="card m-4 text-center">
-  			<div class="card-body p-4">
+		<div class="card m-5 text-center">
+  			<div class="card-body p-5">
     				<h5 class="card-title"><?=$person[$c]?></h5>
     				<p class="card-text"><?=$post[$c]?></p>
   			</div>
