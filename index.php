f2e17686c00ca2fca6b2a5e3464dc0a3252121cf
Manoj Kumar <manojkumar333619@gmail.com>
2020-06-21 14:26:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -237,7 +237,7 @@ Along with these, ample amount of workshops, symposiums, guest lectures and indu
 		<? for($i=0;$i<4;$i++) { ?>
 		<div class="col">
 			<div class="card text-white bg-dark" style="max-width: 18rem;">
-  				<div class="card-header text-white bg-warning"><i class='fa fa-trophy' style='font-size:18px'></i>Header</div>
+  				<div class="card-header text-white bg-warning"><i class='fa fa-trophy' style='font-size:18px'></i>&nbsp;&nbsp;&nbsp;&nbsp;Header</div>
   					<div class="card-body">
     					<h5 class="card-title">Title</h5>
    	 				<p class="card-text">Text Goes here</p>
